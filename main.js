032bb418cad492f999b73ac42b7dfcf3db2175cd
Kingsley <kingsleytorkuebu360x@gmail.com>
2025-09-14 18:32:02 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-import { Client, Account, Databases, Users, ID } from 'node-appwrite';
+import { Client, Account, Databases, Users, ID, Query } from 'node-appwrite';
 
-export default async({ req, res, log, error }) => {
-  
+export default async ({ req, res, log, error }) => {
   const client = new Client()
+    .setEndpoint('https://sfo.cloud.appwrite.io/v1')
+    .setProject(process.env.PROJECT_ID)
+    .setKey(process.env.API_KEY);
+
   const account = new Account(client);
   const database = new Databases(client);
-  const users = new Users(client)
+  const users = new Users(client);
 
-  client
-    .setEndpoint('https://sfo.cloud.appwrite.io/v1') // Your API Endpoint
-    .setProject(process.env.PROJECT_ID) // Your project ID
-    .setKey(process.env.API_KEY); // Your secret API key
-  
   try {
-    const {idToken} = JSON.parse(req.body)
+    const { idToken } = JSON.parse(req.body);
 
     const googleRes = await fetch(`https://oauth2.googleapis.com/tokeninfo?id_token=${idToken}`);
     const googleData = await googleRes.json();
@@ -25,39 +23,40 @@ export default async({ req, res, log, error }) => {
     const email = googleData.email;
     const name = googleData.name;
 
-    try {
-      let appwriteUser = await users.list({
-        queries: [Query.equal("email", email)],
-      });    
-    } catch (error) {
-      let appwriteUser =  await account.create({
+    let appwriteUser;
+    const existing = await users.list({
+      queries: [Query.equal("email", email)],
+    });
+
+    if (existing.total > 0) {
+      appwriteUser = existing.users[0];
+    } else {
+      appwriteUser = await users.create(
         ID.unique(),
         email,
-        "JSJSHJSJHSGJSHGSHJSG8989",
+        "TempPassword123!", // placeholder, can be updated later
         name
-      })
+      );
 
       await database.createDocument(
         process.env.DB_ID,
         process.env.USER_COLLECTION_ID,
         ID.unique(),
         {
-          firstName: googleData.givenName,
-          lastName: googleData.familyName,
+          firstName: googleData.given_name,
+          lastName: googleData.family_name,
           email: googleData.email,
-          avatar: googleData.photo,
-          accountId: ID.unique()
+          avatar: googleData.picture,
+          accountId: appwriteUser.$id
         }
-      )
+      );
     }
 
-    const account = new Account(client);
     const jwt = await account.createJWT();
-
     return res.json({ jwt, user: appwriteUser });
 
   } catch (error) {
-    log(err.message);
-    return res.json({ error: err.message });
+    log(error.message);
+    return res.json({ error: error.message });
   }
-}
+};
